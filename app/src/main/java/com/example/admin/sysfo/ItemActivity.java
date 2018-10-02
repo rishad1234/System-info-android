1b97579a006efc8a96753259e7926a92ef16f21b
Rishadul Islam <rishadulislamkhan@gmail.com>
2018-10-02 13:02:26 +0600

    cpu information added

--- app/src/main/java/com/example/admin/sysfo/ItemActivity.java
+++ app/src/main/java/com/example/admin/sysfo/ItemActivity.java
@@ -8,7 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class ItemActivity extends AppCompatActivity {
 
@@ -24,6 +29,8 @@ public class ItemActivity extends AppCompatActivity {
             String value = bundle.getString("item_name");
             if (value.equals("System")) {
                 addSystemFeatures(textView);
+            } else if(value.equals("CPU")){
+                cpuInfo(textView);
             }
         }
     }
@@ -54,13 +61,13 @@ public class ItemActivity extends AppCompatActivity {
         view.setText(data);
     }
 
-    public static long getTotalMemorySize(){
-        File path = Environment.getDataDirectory();
-        StatFs stat = new StatFs(path.getPath());
-        long blockSize = stat.getBlockSizeLong();
-        long totalBlocks = stat.getBlockCountLong();
-        return totalBlocks * blockSize;
-    }
+//    public static long getTotalMemorySize(){
+//        File path = Environment.getDataDirectory();
+//        StatFs stat = new StatFs(path.getPath());
+//        long blockSize = stat.getBlockSizeLong();
+//        long totalBlocks = stat.getBlockCountLong();
+//        return totalBlocks * blockSize;
+//    }
 
     public static long getAvailableMemorySize(){
         File path = Environment.getDataDirectory();
@@ -68,6 +75,40 @@ public class ItemActivity extends AppCompatActivity {
         long blockSize = stat.getBlockSizeLong();
         long availableBlock = stat.getAvailableBlocksLong();
         return availableBlock * blockSize;
+    }
+
+
+    public void cpuInfo(TextView view){
+        String data = "Cores        : " + getNumCores() + "\n";
+        data += "Threads        : " + getNumCores() * 2 + "\n";
+        data += "Info        : " + getInfo() + "\n";
+        view.setText(data);
+    }
+
+    public static int getNumCores() {
+        return Runtime.getRuntime().availableProcessors();
+    }
+
+    private String getInfo() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("\n").append(Build.CPU_ABI2).append("");
+        if (new File("/proc/cpuinfo").exists()) {
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(new File("/proc/cpuinfo")));
+                String aLine;
+                while ((aLine = br.readLine()) != null) {
+                    sb.append(aLine + "\n");
+                }
+
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+
     }
 
 }
